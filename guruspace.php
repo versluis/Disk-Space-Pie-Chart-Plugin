64ff6f3bfa96e6796cfe6bf841e9dae87d2f2dfd
Jay Versluis <versluis@Jays-MacBook-Pro.local>
2014-01-22 10:49:27 -0500

    added Social Media buttons

--- guruspace.php
+++ guruspace.php
@@ -254,14 +254,42 @@ echo guruspaceServerVersion();
 <?php 
 echo plugins_url('images/guru-header-2013.png', __FILE__);
 ?>" width="300"></a> </p>
+<p><span><!-- Social Buttons -->
+
+<!-- Google+ -->
+<div class="g-follow" data-annotation="bubble" data-height="20" data-href="//plus.google.com/116464794189222694062" data-rel="author"></div>
+
+<!-- Place this tag after the last widget tag. -->
+<script type="text/javascript">
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/platform.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+</script>
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+<!-- Twitter -->
+<a href="https://twitter.com/versluis" class="twitter-follow-button" data-show-count="true">Follow @versluis</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+
+<!-- Facebook -->
+<iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FThe-WP-Guru%2F162188713810370&amp;width&amp;layout=button_count&amp;action=like&amp;show_faces=true&amp;share=true&amp;height=21&amp;appId=186277158097599" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:21px;" allowTransparency="true"></iframe>
+
+
+</span></p>
 <p><a href="http://wpguru.co.uk/2010/12/disk-space-pie-chart-plugin/" target="_blank">Plugin by Jay Versluis</a> | <a href="http://www.peters1.dk/webtools/php/lagkage.php?sprog=en" target="_blank">Pie Chart Script by Rasmus Peters</a> | <a href="http://wphosting.tv" target="_blank">WP Hosting</a> | <a href="http://wpguru.co.uk/say-thanks/" target="_blank">Buy me a Coffee</a> ;-)</p>
 <?php
 
-////////////////
+////////////////////////////////////////
 // TESTING AREA
-echo '<p>TESTING: ';
-echo guruspaceSendMail();
-echo '</p>';
+
+if (WP_DEBUG) {
+	echo '<p>TESTING AREA - displayed only when WP_DEBUG is defined in your installation: ';
+	echo guruspaceSendMail();
+	echo '</p>';
+}
 
 
 ?>
@@ -419,9 +447,11 @@ function guruspaceSendMail() {
 		} 
 	}
 	
-	// testing
-	$testreturn = 'totalspace = ' . $totalspace . ' | freespace = ' . $freespace . ' | usage = '.$usage . ' | recipients = ' . $recepients . ' | headers = '.$headers .' | message = '.$message;
-	return $testreturn;
+	// display test output when debug is enabled
+	if (WP_DEBUG) {
+		$testreturn = 'totalspace = ' . $totalspace . ' | freespace = ' . $freespace . ' | usage = '.$usage . ' | recipients = ' . $recepients . ' | headers = '.$headers .' | message = '.$message;
+		return $testreturn;
+	}
 }
 
 // perform expensive disk read and add it to the database for quick access
